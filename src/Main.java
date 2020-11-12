ca0b7b50c05eaff5923de075abd6c6ffd71ded90
h-kuro <58127378+NNW-hkurosawa@users.noreply.github.com>
2020-11-12 13:36:34 +0900

    [add]文字列の入力

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        System.out.println("式:");
+
+        String inputString = null;
+        try {
+            inputString = bufferedReader.readLine();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(inputString);
+
     }
 }
