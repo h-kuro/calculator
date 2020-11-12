9549b637661fd51ffcc28ebacbba4f5f25335f75
h-kuro <58127378+NNW-hkurosawa@users.noreply.github.com>
2020-11-12 14:34:20 +0900

    [add]文字列中の特定の文字の削除

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +20,8 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        inputString = Objects.requireNonNull(inputString).replace(" ", "");
 
         Pattern pattern = Pattern.compile("[0-9]+|([^0-9])");
         Matcher matcher = pattern.matcher(inputString);
