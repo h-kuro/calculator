ad681fb395a9d81f1c098bebddf807a6cd51e30e
h-kuro <58127378+NNW-hkurosawa@users.noreply.github.com>
2020-11-13 14:41:13 +0900

    [add]四則演算の実装

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,27 +12,69 @@ public class Main {
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        System.out.println("式:");
+        String[] operators = {"+", "-", "*", "/"};
 
-        String inputString = null;
+        while (true) {
+            System.out.print("式:");
+
+            String inputString = null;
+            try {
+                inputString = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            inputString = Objects.requireNonNull(inputString).replace(" ", "");
+
+            if (inputString.equals("end")) break;
+
+            Pattern pattern = Pattern.compile("[0-9]+|([^0-9])");
+            Matcher matcher = pattern.matcher(inputString);
+            int groupCount = matcher.groupCount();
+
+            ArrayList<String> elements = new ArrayList<String>();
+
+            while (matcher.find()) {
+                for (int i = 0; i < groupCount; i++) {
+                    elements.add(matcher.group(i));
+                }
+            }
+
+            int operatorPoint = 2147483647;
+            for (String operator : operators) {
+                int temp = elements.indexOf(operator);
+                if (temp == -1) temp = 2147483647;
+                operatorPoint = Math.min(operatorPoint, temp);
+            }
+
+            if (operatorPoint == 2147483647) {
+                System.out.println("式を入力してください");
+                continue;
+            }
+
+            switch (elements.get(operatorPoint)) {
+                case "+" -> elements.set(operatorPoint, String.valueOf(Integer.parseInt(elements.get(operatorPoint - 1)) + Integer.parseInt(elements.get(operatorPoint + 1))));
+                case "-" -> elements.set(operatorPoint, String.valueOf(Integer.parseInt(elements.get(operatorPoint - 1)) - Integer.parseInt(elements.get(operatorPoint + 1))));
+                case "*" -> elements.set(operatorPoint, String.valueOf(Integer.parseInt(elements.get(operatorPoint - 1)) * Integer.parseInt(elements.get(operatorPoint + 1))));
+                case "/" -> {
+                    if (Integer.parseInt(elements.get(operatorPoint + 1)) == 0) {
+                        elements.set(operatorPoint, "0除算エラー");
+                        break;
+                    }
+                    elements.set(operatorPoint, String.valueOf(Integer.parseInt(elements.get(operatorPoint - 1)) / Integer.parseInt(elements.get(operatorPoint + 1))));
+                }
+            }
+
+            elements.remove(operatorPoint + 1);
+            elements.remove(operatorPoint - 1);
+
+            System.out.println(elements.get(0));
+        }
         try {
-            inputString = bufferedReader.readLine();
             bufferedReader.close();
+            inputStreamReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        inputString = Objects.requireNonNull(inputString).replace(" ", "");
-
-        Pattern pattern = Pattern.compile("[0-9]+|([^0-9])");
-        Matcher matcher = pattern.matcher(inputString);
-        int groupCount = matcher.groupCount();
-
-        while (matcher.find()) {
-            for (int i = 0; i< groupCount; i++){
-                System.out.println(matcher.group(i));
-            }
-        }
-
     }
 }
